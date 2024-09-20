3e6427231cda0cd09c632e5b1f3aa38035bd2a84
MagEk1511 <ekamagerramov@gmail.com>
2024-09-20 20:29:04 +0300

    Added EmailLogController
    *Added endpoint

--- src/main/java/com/example/mailapi/controller/EmailLogController.java
+++ src/main/java/com/example/mailapi/controller/EmailLogController.java
@@ -1,7 +1,34 @@
 package com.example.mailapi.controller;
 
+import com.example.mailapi.model.entity.Email;
+import com.example.mailapi.service.EmailLogService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/emails")
 public class EmailLogController {
+
+
+    private final EmailLogService emailLogService;
+
+    public EmailLogController(EmailLogService emailLogService) {
+        this.emailLogService = emailLogService;
+    }
+
+    @GetMapping(params = "id")
+    public Optional<Email> email(@RequestParam Long id) {
+        return emailLogService.getEmailById(id);
+    }
+
+    @GetMapping
+    public List<Email> emails() {
+        return emailLogService.getAllEmails();
+    }
+
 }
--- src/main/java/com/example/mailapi/service/EmailLogService.java
+++ src/main/java/com/example/mailapi/service/EmailLogService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmailLogService {
@@ -25,6 +26,10 @@ public class EmailLogService {
         email.setStatus(status);
 
         emailRepository.save(email);
+    }
+
+    public Optional<Email> getEmailById(Long id) {
+        return emailRepository.findById(id);
     }
 
     public List<Email> getAllEmails() {
